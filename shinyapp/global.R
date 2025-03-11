4a47c5d761172658c571b58bd6be24cb656f34cb
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-03-11 10:46:04 +0100

    environment cleanup

--- shinyapp/global.R
+++ shinyapp/global.R
@@ -7,9 +7,6 @@ library(bslib)
 # -- Set environment
 DEBUG <- as.logical(Sys.getenv("DEBUG"))
 
-#WS_URL <- Sys.getenv("WS_URL")
-#WS_BASE_ROUTE <- Sys.getenv("WS_BASE_ROUTE")
-
 
 # -- Source code
 for(nm in list.files("R", pattern = ".R", full.names = T, recursive = T))
